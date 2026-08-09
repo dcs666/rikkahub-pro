597dfe56d6dee519709ec18e6d65cf476d678b3c
rikkahub-turbo <rikkahub-turbo@users.noreply.github.com>
2026-08-09 07:03:08 +0000

    chore(release): v1.8.0-turbo (2.5.6/185)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "dev.nebula.turbo"
         minSdk = 26
         targetSdk = 37
-        versionCode = 184
-        versionName = "2.5.5"
+        versionCode = 185
+        versionName = "2.5.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
